e174fa52bab026f2c3881b707bd0d98d3153ce11
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-08-12 14:59:24 +0200

    tekst oppdatering

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ let strHtml;
                 <a href="https://irobin93.github.io/VSCode/funksjonMedParameter.html" target="_blank">Funksjon med parameter</a>
                 </li>
                 <li>
-                <a href="https://irobin93.github.io/VSCode/merOmVariabler.html" target="_blank">Mer om variabler</a>
+                <a href="https://irobin93.github.io/VSCode/merOmVariabler.html" target="_blank">Farge lek</a>
                 </li>
              </ul> ` 
         }
